f44812898b552529e12dd1ec5ebc3dc8227fd579
zzhaneka <zhanaibek.zhaniya@mail.ru>
2025-04-05 08:54:43 +0500

    feat: добавлена имитация миниатюр и поведение по клику

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,29 @@ import image.Image;
 import image.RealImage;
 import image.ProxyImage;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("== Пример с использованием Proxy ==");
+        System.out.println("== Страница объекта недвижимости ==");
 
-        Image thumb1 = new ProxyImage("house1.jpg");
-        Image thumb2 = new ProxyImage("house2.jpg");
+        Image preview1 = new ProxyImage("villa-south.jpg");
+        Image preview2 = new ProxyImage("loft-center.jpg");
 
-        System.out.println("-- Отображаем миниатюры (ничего не грузим) --");
+        // эмуляция миниатюр
+        System.out.println("\n[Миниатюры загружены]");
+        System.out.println("Показ миниатюры: villa-south.jpg");
+        System.out.println("Показ миниатюры: loft-center.jpg");
 
-        // Покажем полные изображения только по запросу
-        System.out.println("\n-- Клик по первому изображению --");
-        thumb1.display();
+        // пользователь кликает на первое изображение
+        System.out.println("\n[Пользователь нажал на первую миниатюру]");
+        preview1.display();
 
-        System.out.println("\n-- Повторный клик по первому изображению --");
-        thumb1.display();
+        // повторный клик — изображение уже в кэше
+        System.out.println("\n[Повторный клик на ту же картинку]");
+        preview1.display();
 
-        System.out.println("\n-- Клик по второму изображению --");
-        thumb2.display();
+        // клик на вторую
+        System.out.println("\n[Клик по второму объекту]");
+        preview2.display();
     }
 }
